d91c6d3224f884972b5e6a5433e25de44bd196b0
scatler <38757410+scatler@users.noreply.github.com>
2020-06-02 12:03:34 +0300

    final solution regarding 302 error

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbwzoj6S-mGYkDI6TTNGDC1N1-UHZ8_427QZkYbvddg5ex3aWf0j/exec', data)
+             $.post('https://script.google.com/macros/s/AKfycbwzoj6S-mGYkDI6TTNGDC1N1-UHZ8_427QZkYbvddg5ex3aWf0j/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -231,10 +231,15 @@ $(document).ready(function () {
                 .fail(function (data) {
                     console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                })
+                .error(function(data) {
+                    //handle 302 code (which is actually ok)
+                    console.log(data);
+                    $('#alert-wrapper').html('');
+                    $('#rsvp-modal').modal('show');
                 });
         }
     });
-
 });
 
 /********************** Extras **********************/
